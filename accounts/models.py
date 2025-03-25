1465fa1c8f172c196f289267545b7775b2344349
harsh.r <r.harsh20.r@gmail.com>
2025-03-26 00:30:44 +0530

    upd 2

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class UserProfile(models.Model):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.email
