653f4da647a0a3e4a4f42e354a63b3fc9a3a1d2a
harsh.r <r.harsh20.r@gmail.com>
2025-03-27 00:41:37 +0530

    organisation updtd migrated admin registered

--- organization/admin.py
+++ organization/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Employee, MoodHistory, Activity, Reward, DailyStreak, Calendar, UpcomingTask, DetailedSummary
 
-# Register your models here.
+# Register models to appear in Django Admin
+admin.site.register(Employee)
+admin.site.register(MoodHistory)
+admin.site.register(Activity)
+admin.site.register(Reward)
+admin.site.register(DailyStreak)
+admin.site.register(Calendar)
+admin.site.register(UpcomingTask)
+admin.site.register(DetailedSummary)
