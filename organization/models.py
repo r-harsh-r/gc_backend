0416af8570aa47946bd5fe1f95cba1a3d9352355
harsh.r <r.harsh20.r@gmail.com>
2025-03-26 22:36:26 +0530

    login organisation and dashboard

--- organization/models.py
+++ organization/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
-class Employee(models.Model):
-    employee_id = models.IntegerField(unique=True)
-    name = models.CharField(max_length=100)
-    post = models.CharField(max_length=50)
-    last_mod = models.IntegerField()
-    load = models.IntegerField()
-    productivity = models.IntegerField()
+# class Employee(models.Model):
+#     employee_id = models.IntegerField(unique=True)
+#     name = models.CharField(max_length=100)
+#     post = models.CharField(max_length=50)
+#     last_mod = models.IntegerField()
+#     load = models.IntegerField()
+#     productivity = models.IntegerField()
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
 
 
 from django.db import models
